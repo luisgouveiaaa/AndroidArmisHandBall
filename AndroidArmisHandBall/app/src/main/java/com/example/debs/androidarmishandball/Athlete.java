2fefbc406b80235badc30b7098976276694a4ca3
Joao Westerberg [1150447] <1150447@isep.ipp.pt>
2017-08-28 13:12:16 +0100

    Changes to DTOs

--- AndroidArmisHandBall/app/src/main/java/com/example/debs/androidarmishandball/Athlete.java
+++ AndroidArmisHandBall/app/src/main/java/com/example/debs/androidarmishandball/Athlete.java
@@ -5,4 +5,92 @@ package com.example.debs.androidarmishandball;
  */
 
 public class Athlete {
+
+    private int pk;
+    private byte[] photo;
+    private String name;
+    private String gender;
+    private int age;
+    private String nationality;
+    private String clubName;
+    private int teamPk;
+
+    public Athlete(){
+
+    }
+
+    public Athlete(int pk, byte[] photo , String name , String gender , int age , String nationality , String clubName , int teamPk){
+        this.pk = pk;
+        this.photo = photo;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.nationality = nationality;
+        this.clubName = clubName;
+        this.teamPk = teamPk;
+    }
+
+    public int getPk() {
+        return pk;
+    }
+
+    public void setPk(int pk) {
+        this.pk = pk;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getClubName() {
+        return clubName;
+    }
+
+    public void setClubName(String clubName) {
+        this.clubName = clubName;
+    }
+
+    public int getTeamPk() {
+        return teamPk;
+    }
+
+    public void setTeamPk(int teamPk) {
+        this.teamPk = teamPk;
+    }
 }
---
+++ AndroidArmisHandBall/app/src/main/java/com/example/debs/androidarmishandball/GoalRecord.java
@@ -0,0 +1,64 @@
+package com.example.debs.androidarmishandball;
+
+public class GoalRecord {
+
+    private int pk ;
+    private int team ;
+    private String athleteName ;
+    private byte[] athletePhoto ;
+    private byte count ;
+
+    public GoalRecord(){
+
+    }
+
+    public GoalRecord(int pk, int team , String athleteName , byte[] athletePhoto , byte count)
+    {
+        this.pk = pk;
+        this.team = team; //team is 0 to home, 1 to visitor and 2 if different
+        this.athleteName = athleteName;
+        this.athletePhoto = athletePhoto;
+        this.count = count;
+    }
+
+    public int getPk() {
+        return pk;
+    }
+
+    public void setPk(int pk) {
+        this.pk = pk;
+    }
+
+    public int getTeam() {
+        return team;
+    }
+
+    public void setTeam(int team) {
+        this.team = team;
+    }
+
+    public String getAthleteName() {
+        return athleteName;
+    }
+
+    public void setAthleteName(String athleteName) {
+        this.athleteName = athleteName;
+    }
+
+    public byte[] getAthletePhoto() {
+        return athletePhoto;
+    }
+
+    public void setAthletePhoto(byte[] athletePhoto) {
+        this.athletePhoto = athletePhoto;
+    }
+
+    public byte getCount() {
+        return count;
+    }
+
+    public void setCount(byte count) {
+        this.count = count;
+    }
+
+}
